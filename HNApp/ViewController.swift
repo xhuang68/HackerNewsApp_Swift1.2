bcdc959ca12425cebc5cbc6f9799836bd772b330
xiao <henryhuang1989@gmail.com>
2015-10-01 13:50:49 -0700

    Initial Commit

---
+++ HNApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HNApp
+//
+//  Created by Henry Huang on 10/1/15.
+//  Copyright (c) 2015 XiaoHuang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
